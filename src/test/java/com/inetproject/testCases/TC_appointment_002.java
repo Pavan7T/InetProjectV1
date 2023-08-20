fda65059f3da720181e71fb86f58f4c99865c5ce
Pavan7T <p1unofficial43@gmail.com>
2023-08-20 12:06:20 +0530

    new test case

---
+++ src/test/java/com/inetproject/testCases/TC_appointment_002.java
@@ -0,0 +1,13 @@
+package com.inetproject.testCases;
+
+import org.testng.annotations.Test;
+
+@Test
+public class TC_appointment_002 {
+
+	public static void appointmentlist() {
+
+		System.out.println("patientlist");
+
+	}
+}
